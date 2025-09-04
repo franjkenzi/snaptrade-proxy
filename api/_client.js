18f0be4778d1db8d7c0fa5d7d03de3b6e59579ce
franjkenzi <kovacevicluka886@gmail.com>
2025-09-04 22:33:58 +0200

    Create _client.js

---
+++ api/_client.js
@@ -0,0 +1,8 @@
+import { Snaptrade } from "snaptrade-typescript-sdk";
+
+const snaptrade = new Snaptrade({
+  clientId: process.env.SNAP_CLIENT_ID,
+  consumerKey: process.env.SNAP_CONSUMER_KEY,
+});
+
+export default snaptrade;
