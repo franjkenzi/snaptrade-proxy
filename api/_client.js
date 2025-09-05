d49d76c8c9dc2ea56ae90f0134610661fed07108
franjkenzi <kovacevicluka886@gmail.com>
2025-09-05 12:47:18 +0200

    Update _client.js

--- api/_client.js
+++ api/_client.js
@@ -1,8 +1,22 @@
-import { Snaptrade } from "snaptrade-typescript-sdk";
+// api/_client.js
+import {
+  createConfiguration,
+  APIStatusApi,
+  AuthenticationApi,
+  AccountsApi,
+  HoldingsApi, // 👈 DODATO
+} from "snaptrade-typescript-sdk"; // koristi isti paket kao i do sada kod tebe
 
-const snaptrade = new Snaptrade({
-  clientId: process.env.SNAP_CLIENT_ID,
+const config = createConfiguration({
   consumerKey: process.env.SNAP_CONSUMER_KEY,
+  clientId: process.env.SNAP_CLIENT_ID,
 });
+
+const snaptrade = {
+  apiStatus: new APIStatusApi(config),
+  authentication: new AuthenticationApi(config),
+  accountInformation: new AccountsApi(config),
+  holdings: new HoldingsApi(config), // 👈 DODATO
+};
 
 export default snaptrade;
