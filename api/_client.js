2cf5ffc82a7c91c67b11ea85f8179ac74b0d3972
franjkenzi <kovacevicluka886@gmail.com>
2025-09-05 13:12:01 +0200

    Update _client.js

--- api/_client.js
+++ api/_client.js
@@ -1,10 +1,28 @@
 // api/_client.js
-import Snaptrade from "snaptrade-typescript-sdk";
 
-const snaptrade = new Snaptrade({
-  clientId: process.env.SNAP_CLIENT_ID,
+import pkg from "snaptrade-typescript-sdk";
+
+// CommonJS -> iz default importa vadimo potrebne klase/funkcije
+const {
+  createConfiguration,
+  APIStatusApi,
+  AuthenticationApi,
+  AccountsApi,
+  HoldingsApi,
+} = pkg;
+
+const config = createConfiguration({
   consumerKey: process.env.SNAP_CONSUMER_KEY,
+  clientId: process.env.SNAP_CLIENT_ID,
 });
+
+// Jedinstveni “client” koji izvozimo
+const snaptrade = {
+  apiStatus: new APIStatusApi(config),
+  authentication: new AuthenticationApi(config),
+  accountInformation: new AccountsApi(config),
+  holdings: new HoldingsApi(config),
+};
 
 export default snaptrade;
 
